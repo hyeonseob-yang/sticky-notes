b49bb9ddc510808531e23a04dae19dcacb4ab254
hya81 <hyeonseob.yang@gmail.com>
2026-07-10 14:59:42 -0700

    Add ref support to TrashZone for hit-testing

--- src/components/TrashZone.tsx
+++ src/components/TrashZone.tsx
@@ -1,9 +1,12 @@
+import type { Ref } from 'react'
+
 interface TrashZoneProps {
   isActive: boolean
+  ref?: Ref<HTMLDivElement>
 }
 
-export function TrashZone({ isActive }: TrashZoneProps) {
+export function TrashZone({ isActive, ref }: TrashZoneProps) {
   const className = isActive ? 'trash-zone trash-zone--active' : 'trash-zone'
 
-  return <div data-testid="trash-zone" className={className} />
+  return <div ref={ref} data-testid="trash-zone" className={className} />
 }
